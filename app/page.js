757bbb8638d54a331f1c89a73766b3aab92a2ba9
Pratik Kumar <kumarpratik553@gmail.com>
2025-12-09 16:05:38 +0530

    Updated NextJS version

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Login from "@/components/Login";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main id="hero">
       <div className="hero-img">
-        <img alt="hero-img" src="hero-img.jpeg" />
+        <Image alt="hero-img" width={2000} height={2000} src="/hero-img.jpeg" />
       </div>
       <div className="hero-login">
         <Login />
